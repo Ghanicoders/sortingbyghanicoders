9ec82ad95e8d263cebe7c512bb0c0530f7f12aeb
Ghanicoders <musarrafghani8@gmail.com>
2023-08-29 22:32:34 +0530

    allset for buuble sort this will help us understanfd buuble sorting

--- bubbblesorting.java
+++ bubbblesorting.java
@@ -1,5 +1,27 @@
 import java.util.*;
 class bubble{
+
+    public static void sorting(int arr []) {
+        if(arr.length == 0){
+            return;
+
+        }
+
+        //this two loop wiiil sorting here
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr [j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        // and this loop will print the  all element after sorting
+        for(int i=0; i<arr.length; i++){
+            System.out.print("-> "+arr[i]);
+        }
+    }
     public static void main (String args []){
         System.out.println("hello world");
         //i will make here a array in size of 5
@@ -17,7 +39,14 @@ class bubble{
         }
         //System elements of array secsesfully inputed
         System.out.println("after arrr.element inputed");
+
          // befor sorting array elements looks like 
+         for(int i=0; i<arr.length; i++){
+            System.out.print("-> "+arr[i]);
+         }
+         System.out.println();
+         sorting(arr);
+         System.out.println();
          for(int i=0; i<arr.length; i++){
             System.out.print("-> "+arr[i]);
          }
